7f790006c92f5e5bd15473aac9ad85479991866d
MhmdShamii <mhmddshamii@gmail.com>
2026-01-01 23:14:58 +0200

    Switch to mysql2 for Railway

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*", // or your frontend URL
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
